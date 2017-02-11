b1a106a4cc1b49d9910e75f298d4f7c9e63fd28a
vseledkin <vseledkin@iSkynet.local>
2017-02-11 18:26:39 +0300

    sgd solver

---
+++ sgd.go
@@ -0,0 +1,18 @@
+package gortex
+
+type SGDSolver struct{}
+
+func NewSGDSolver() *SGDSolver {
+	s := new(SGDSolver)
+	return s
+}
+
+func (this *SGDSolver) Step(model map[string]*Matrix, step_size float64) {
+	for _, m := range model {
+		for i := range m.W {
+			m.W[i] += -step_size * m.DW[i]
+			m.DW[i] = 0 // reset gradients for next iteration
+		}
+	}
+	return
+}
