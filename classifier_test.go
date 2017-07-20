147e0d0d1ec9839ad2f54b4db75bd0060a141841
viacheslav seledkin <vseledkin@gmail.com>
2017-07-20 19:43:22 +0300

    add classifier test

---
+++ classifier_test.go
@@ -0,0 +1,107 @@
+package gortex
+
+import (
+	"fmt"
+	"testing"
+	"time"
+	"math/rand"
+)
+
+func TestClassifier(t *testing.T) {
+	// maintain random seed
+	rand.Seed(time.Now().UnixNano())
+	trainFile := "train.txt"
+	dic, e := CharDictionaryFromFile(trainFile, CharSplitter{})
+	if e != nil {
+		t.Fatal(e)
+	}
+	hidden_size := 128
+	fmt.Printf("Dictionary has %d tokens\n", dic.Len())
+	fmt.Printf("%s\n", dic)
+
+	s := NewSolver() // the Solver uses RMSPROP
+
+	encoder := MakeGRU(dic.Len(), hidden_size, 1)
+	Who := RandXavierMat(2, hidden_size)
+	Bho := RandXavierMat(2, 1)
+	// define model parameters
+	encoderModel := encoder.GetParameters("Encoder")
+	encoderModel["out"] = Who
+	encoderModel["outBias"] = Bho
+
+	count := 0
+	ma_d := NewMovingAverage(256)
+
+	learning_rate := float32(0.001)
+	anneal_rate := float32(0.999)
+
+	CharClassifierSampleVisitor(trainFile, 10, true, CharSplitter{}, dic, func(x []uint, label string) {
+		// read sample
+		sample := ""
+		for i := range x {
+			sample += dic.TokenByID(x[i])
+		}
+		G := &Graph{NeedsBackprop: true}
+		ht := Mat(hidden_size, 1) // vector of zeros
+		// encode sequence into z
+		for i := range x {
+			oneHot := Mat(dic.Len(), 1)
+			oneHot.W[x[i]] = 1.0
+			ht, _ = encoder.Step(G, oneHot, ht)
+		}
+		logits := G.Add(G.Mul(Who, ht),Bho)
+		var target uint
+		if label == "__label__Task" {
+			target = 1
+		}
+		predicted_class, _ := MaxIV(Softmax(logits))
+		cost, prob := G.Crossentropy(logits, target)
+
+		G.Backward()
+		//ScaleGradient(encoderModel, 1.0/float32(len(x)))
+		s.Step(encoderModel, learning_rate, 0.0, 5.0)
+
+		count++
+
+		ma_d.Add(cost)
+
+		avg_cost := ma_d.Avg()
+		if count%50 == 0 {
+			fmt.Printf("label: [%s] [%d] ? %d %f\n", label, target, predicted_class, prob)
+			fmt.Printf("encoded: [%s]\n", sample)
+			fmt.Printf("step: %d loss: %f lr: %f\n", count, avg_cost, learning_rate)
+			learning_rate = learning_rate * anneal_rate
+		}
+
+			if count%1000 == 0 { // TEST
+				var trueLabels, predictedLabels []uint
+				CharClassifierSampleVisitor("test.txt", 10, false, CharSplitter{}, dic, func(x []uint, label string) {
+					// read sample
+					sample := ""
+					for i := range x {
+						sample += dic.TokenByID(x[i])
+					}
+					G := &Graph{NeedsBackprop: false}
+					ht := Mat(hidden_size, 1) // vector of zeros
+					// encode sequence into z
+					for i := range x {
+						oneHot := Mat(dic.Len(), 1)
+						oneHot.W[x[i]] = 1.0
+						ht, _ = encoder.Step(G, oneHot, ht)
+					}
+					logits := G.Add(G.Mul(Who, ht),Bho)
+					var target uint
+					if label == "__label__Task" {
+						target = 1
+					}
+					trueLabels = append(trueLabels,target)
+
+					predicted_class, _ := MaxIV(Softmax(logits))
+					predictedLabels = append(predictedLabels,predicted_class)
+					//fmt.Printf("sample %d %d %s\n", predicted_class, target, sample)
+				})
+				F1, message := F1Score(trueLabels,predictedLabels,[]string{"Neutral","Task"},nil)
+				fmt.Printf("\n\nF1: %f %s\n\n", F1, message)
+			}
+	})
+}
--- corpus.go
+++ corpus.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"os"
 	"strings"
+	"fmt"
 )
 
 func CharSampleVisitor(file string, minLen uint, tokenizer Tokenizer, dictionary *Dictionary, visitor func(sample []uint)) error {
@@ -30,6 +31,45 @@ func CharSampleVisitor(file string, minLen uint, tokenizer Tokenizer, dictionary
 			}
 			if len(sample) > int(minLen) {
 				visitor(sample)
+			}
+		}
+	}
+	f.Close()
+	return nil
+}
+func CharClassifierSampleVisitor(file string, minLen uint, rewind bool, tokenizer Tokenizer, dictionary *Dictionary, visitor func(sample []uint, label string)) error {
+	f, e := os.Open(file)
+	if e != nil {
+		return e
+	}
+	r := bufio.NewReader(f)
+	for {
+		line, e := r.ReadString('\n')
+		if e != nil {
+			if rewind {
+				f.Seek(0, 0)
+				line, e = r.ReadString('\n')
+				if e != nil {
+					break
+				}
+			} else {
+				break
+			}
+ 		}
+		line = strings.TrimSpace(line)
+		if len(line) > 0 {
+			index := strings.LastIndex(line, " __label__")
+			if index == -1 {
+				panic(fmt.Errorf("Wrong classifier sample [%s]", line))
+			}
+			label := line[index+1:]
+			terms := tokenizer.Split(line[:index])
+			sample := make([]uint, len(terms))
+			for i, term := range terms {
+				sample[i] = dictionary.IDByToken(term)
+			}
+			if len(sample) > int(minLen) {
+				visitor(sample, label)
 			}
 		}
 	}
