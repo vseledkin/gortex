5478950ae4583d690f0a41a18639b5f7035b927d
viacheslav seledkin <vseledkin@gmail.com>
2017-08-10 09:42:49 +0300

    vae model

---
+++ vae.go
@@ -0,0 +1,72 @@
+package gortex
+
+import "fmt"
+
+// Gated recurrent unit
+
+type VAE struct {
+	z_size int
+	W      *Matrix
+	B      *Matrix
+
+	WM *Matrix
+	BM *Matrix
+
+	WD *Matrix
+	BD *Matrix
+}
+
+func MakeVae(x_size, z_size int) *VAE {
+	vae := new(VAE)
+	vae.z_size = z_size
+	vae.W = RandXavierMat(z_size, x_size)
+	vae.B = RandXavierMat(z_size, 1)
+
+	vae.WM = RandXavierMat(z_size, z_size)
+	vae.BM = RandXavierMat(z_size, 1)
+
+	vae.WD = RandXavierMat(z_size, z_size)
+	vae.BD = RandXavierMat(z_size, 1)
+
+	return vae
+}
+
+func (vae *VAE) GetParameters(namespace string) map[string]*Matrix {
+	return map[string]*Matrix{
+		namespace + "_W": vae.W,
+		namespace + "_B": vae.B,
+
+		namespace + "_WM": vae.WM,
+		namespace + "_BM": vae.BM,
+
+		namespace + "_WD": vae.WD,
+		namespace + "_BD": vae.BD,
+	}
+}
+
+func (vae *VAE) SetParameters(namespace string, parameters map[string]*Matrix) error {
+	for k, v := range vae.GetParameters(namespace) {
+		fmt.Printf("Look for %s parameters\n", k)
+		if m, ok := parameters[k]; ok {
+			fmt.Printf("Got %s parameters\n", k)
+			copy(v.W, m.W)
+		} else {
+			return fmt.Errorf("Model geometry is not compatible, parameter %s is unknown", k)
+		}
+	}
+	return nil
+}
+
+func (vae *VAE) Step(g *Graph, x *Matrix) (sample, mean, dev *Matrix) {
+	// make VAE computation graph
+	//println("x", x.Rows, x.Columns)
+	//println("B", vae.B.Rows, vae.B.Columns)
+	xz := g.Tanh(g.Add(g.Mul(vae.W, x), vae.B))
+	mean = g.Add(g.Mul(vae.WM, xz), vae.BM)
+	dev = g.Sigmoid(g.Add(g.Mul(vae.WD, xz), vae.BD)) // must be positive
+	// sample random vector from normal 0 1 distribution
+	eps := RandMat(vae.z_size, 1)
+	// sample exemplar from generated distribution
+	sample = g.Add(mean, g.EMul(dev, eps))
+	return
+}
---
+++ vae_test.go
@@ -0,0 +1,133 @@
+package gortex
+
+import (
+	"fmt"
+	"math/rand"
+	"testing"
+	"time"
+
+	"github.com/vseledkin/gortex/assembler"
+	"github.com/gizak/termui"
+)
+
+func TestVae(t *testing.T) {
+
+		// maintain random seed
+		rand.Seed(time.Now().UnixNano())
+		trainFile := "input.txt"
+		dic, e := CharDictionaryFromFile(trainFile, CharSplitter{})
+		if e != nil {
+			t.Fatal(e)
+		}
+		hidden_size := 128
+		embedding_size := 128
+		z_size := 128
+		fmt.Printf("Dictionary has %d tokens\n", dic.Len())
+		fmt.Printf("%s\n", dic)
+
+		s := NewSolver()                                  // the Solver uses RMSPROP
+		LookupTable := RandMat(embedding_size, dic.Len()) // Lookup Table matrix
+		encoder := MakeOutputlessLSTM(embedding_size, hidden_size)
+		encoder.ForgetGateTrick(2.0)
+		vae := MakeVae(hidden_size, z_size)
+		decoder := MakeLSTM(z_size, hidden_size, dic.Len())
+		decoder.ForgetGateTrick(2.0)
+
+		// define model parameters
+		encoderModel := encoder.GetParameters("Encoder")
+		encoderModel["LookupTable"] = LookupTable
+		vaeModel := vae.GetParameters("VAE")
+		decoderModel := decoder.GetParameters("Decoder")
+
+		count := 0
+		ma_d := NewMovingAverage(50)
+		//batch_size := 8
+
+		max_len := 32
+
+		var e_steps, d_steps float32
+		learning_rate := float32(0.001)
+		anneal_rate := float32(0.999)
+
+		CharSampleVisitor(trainFile, 10, CharSplitter{}, dic, func(x []uint) {
+			if len(x) > max_len {
+				return
+			}
+			// read sample
+			sample := ""
+			for i := range x {
+				sample += dic.TokenByID(x[i])
+			}
+			G := &Graph{NeedsBackprop: true}
+			ht := Mat(hidden_size, 1).OnesAs() // vector of zeros
+			ct := Mat(hidden_size, 1).OnesAs() // vector of zeros
+
+			// encode sequence into z
+			for i := range x {
+				e_steps++
+				embedding := G.Lookup(LookupTable, int(x[i]))
+				ht, ct = encoder.Step(G, embedding, ht, ct)
+			}
+			distribution, mean, dev := vae.Step(G, ct)
+			// decode sequence from z
+
+			var logit *Matrix
+			cost := float32(0)
+
+			decoded := ""
+			ht = Mat(hidden_size, 1).OnesAs() // vector of zeros
+			ct = Mat(hidden_size, 1).OnesAs() // vector of zeros
+			for i := range x {
+				d_steps++
+				ht, ct, logit = decoder.Step(G, distribution, ht, ct)
+				c, _ := G.Crossentropy(logit, x[i])
+				cid, _ := MaxIV(Softmax(logit))
+				decoded += dic.TokenByID(cid)
+				cost += c
+			}
+			cost /= float32(len(x))
+			G.Backward()
+			ScaleGradient(encoderModel, 1/e_steps)
+			ScaleGradient(decoderModel, 1/d_steps)
+			s.Step(encoderModel, learning_rate, 0.0, 5.0)
+			s.Step(vaeModel, learning_rate, 0.0, 0.0)
+			s.Step(decoderModel, learning_rate, 0.0, 5.0)
+			d_steps = 0
+			e_steps = 0
+
+			count++
+			//if count > 0 && count%batch_size == 0 {
+			//d_cost /= d_steps
+			//g_cost /= g_steps
+			ma_d.Add(cost)
+			//if sample != decoded {
+			//}
+			avg_cost := ma_d.Avg()
+			if count%150 == 0 {
+				fmt.Printf("\ndecoded: [%s]\n", decoded)
+				fmt.Printf("encoded: [%s]\n", sample)
+				fmt.Printf("step: %d loss: %f lr: %f\n", count, avg_cost, learning_rate)
+				fmt.Printf("dev: %f mean: %f\n", assembler.L1(dev.W), assembler.L1(mean.W))
+
+				learning_rate = learning_rate * anneal_rate
+			}
+			/*
+				if count%100 == 0 { // print some model generated text
+					learning_rate = learning_rate * anneal_rate
+					// sample noise
+					//z := RandMat(z_size, 1)
+					fmt.Printf("MODEL GENERATED TEXT: ")
+					G := Graph{NeedsBackprop: false}
+					ht := Mat(g_hidden_size, 1) // vector of zeros
+					var logit *Matrix
+					for i := 0; i < max_len; i++ {
+						ht, logit = generator.Step(&G, z, ht)
+						term_id, _ := MaxIV(Softmax(logit))
+						fmt.Printf("%s", dic.TokenByID(term_id))
+					}
+					fmt.Printf("\n")
+				}*/
+		})
+
+	termui.Loop()
+}
